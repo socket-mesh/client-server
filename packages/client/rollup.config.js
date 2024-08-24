12780dadee8b5b7f923639246746a93b7d02df19
Greg Kimmy <greg.kimmy@gmail.com>
2024-08-23 23:24:56 -0500

    fix: added bundle types

--- packages/client/rollup.config.js
+++ packages/client/rollup.config.js
@@ -2,23 +2,38 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import { dts } from "rollup-plugin-dts";
+import { del } from '@kineticcafe/rollup-plugin-delete';
 
 /** @type {import('rollup').RollupOptions} */
-export default {
-	input: 'src/index.ts',
-	output: {
-		file: 'socket-mesh-client.js',
-		format: 'es'
+export default [
+	{
+		input: 'src/index.ts',
+		output: {
+			file: './bundle/socket-mesh-client.js',
+			format: 'es'
+		},
+		plugins: [
+			typescript({
+				tsconfig: './tsconfig.build.json'
+			}),
+			commonjs(),
+			nodeResolve({
+				preferBuiltins: false,
+				browser: true
+			}),
+			//terser()
+		]
 	},
-	plugins: [
-		typescript({
-			tsconfig: './tsconfig.build.json'
-		}),
-		commonjs(),
-		nodeResolve({
-			preferBuiltins: false,
-			browser: true
-		}),
-		//terser()
-	]
-};
+	{
+		input: "./bundle/dist/index.d.ts",
+		output: [{ file: "bundle/socket-mesh-client.d.ts", format: "es" }],
+		plugins: [
+			dts(),
+			del({
+				targets: 'bundle/dist',
+				hook: 'writeBundle'
+			})
+		]
+	}
+];
