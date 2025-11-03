77944757c34cc58b6a4309a36f1d1ae4b29e410f
Greg Kimmy <greg.kimmy@gmail.com>
2025-11-02 21:05:05 -0600

    fix: fixed lint errors

--- packages/consumable-stream/src/index.ts
+++ packages/consumable-stream/src/index.ts
@@ -1,22 +1,29 @@
-export abstract class ConsumableStream<T, TReturn = any> implements AsyncIterator<T, TReturn | undefined>, AsyncIterable<T> {
+export interface Consumer<T, TReturn = any> {
+	next(): Promise<IteratorResult<T, TReturn>>,
+	return(): Promise<IteratorResult<T, TReturn>>
+}
+
+export abstract class ConsumableStream<T, TReturn = any> implements AsyncIterable<T>, AsyncIterator<T, TReturn | undefined> {
+	abstract createConsumer(timeout?: number): Consumer<T, TReturn | undefined>;
+
 	async next(timeout?: number): Promise<IteratorResult<T, TReturn | undefined>> {
-		let asyncIterator = this.createConsumer(timeout);
-		let result = await asyncIterator.next();
+		const asyncIterator = this.createConsumer(timeout);
+		const result = await asyncIterator.next();
 		asyncIterator.return();
 		return result;
 	}
 
 	async once(timeout?: number): Promise<T | TReturn> {
-		let result = await this.next(timeout);
+		const result = await this.next(timeout);
 
 		if (result.done) {
 			// If stream was ended, this function should never resolve unless
 			// there is a timeout; in that case, it should reject early.
 			if (timeout == null) {
 				return await new Promise(() => {});
-				//const error = new Error('Stream ended before yielding a value');
-				//error.name = 'StreamEndedError';
-				//throw error;
+				// const error = new Error('Stream ended before yielding a value');
+				// error.name = 'StreamEndedError';
+				// throw error;
 			} else {
 				const error = new Error(
 					'Stream consumer operation timed out early because stream ended'
@@ -29,14 +36,7 @@ export abstract class ConsumableStream<T, TReturn = any> implements AsyncIterato
 		return result.value;
 	}
 
-	abstract createConsumer(timeout?: number): Consumer<T, TReturn | undefined>
-
 	[Symbol.asyncIterator](): AsyncIterator<T, TReturn | undefined> {
 		return this.createConsumer();
 	}
-}
-
-export interface Consumer<T, TReturn = any> {
-	next(): Promise<IteratorResult<T, TReturn>>;
-	return(): Promise<IteratorResult<T, TReturn>>;
 }
