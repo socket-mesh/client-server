54d1fdd752e1ff86296faf3d9f83f28671f98050
Greg Kimmy <greg.kimmy@gmail.com>
2025-11-03 20:52:15 -0600

    fix: corrected return types

--- packages/local-storage/src/index.ts
+++ packages/local-storage/src/index.ts
@@ -7,13 +7,13 @@ export class LocalStorage implements Storage {
 		this._data = {};
 	}
 
-	clear() {
+	clear(): void {
 		for (const key of Object.keys(this._data)) {
 			delete this._data[key];
 		}
 	}
 
-	getItem(key: string) {
+	getItem(key: string): null | string {
 		if (Object.prototype.hasOwnProperty.call(this._data, key)) {
 			return String(this._data[key]);
 		}
@@ -24,20 +24,20 @@ export class LocalStorage implements Storage {
 		return Object.prototype.hasOwnProperty.call(this._data, key);
 	}
 
-	key(i: number): string {
+	key(i: number): null | string {
 		i = i || 0;
-		return Object.keys(this._data)[i];
+		return Object.keys(this._data)[i] ?? null;
 	}
 
 	get length(): number {
 		return Object.keys(this._data).length;
 	}
 
-	removeItem(key: string) {
+	removeItem(key: string): void {
 		delete this._data[key];
 	}
 
-	setItem(key: string, val: any) {
+	setItem(key: string, val: any): void {
 		this._data[key] = String(val);
 	}
 }
