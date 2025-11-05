40be63b14077ac2d940487782728d682c6ab8bc9
Greg Kimmy <greg.kimmy@gmail.com>
2025-11-04 20:25:47 -0600

    fix: updated code to meet lint requirements

--- packages/core/src/maps/method-map.ts
+++ packages/core/src/maps/method-map.ts
@@ -1,33 +1,32 @@
-export type RemoveIndexSignature<T> = {  
-  [K in keyof T as string extends K
-    ? never
-    : number extends K
-      ? never
-      : symbol extends K
-        ? never
-        : K
-  ]: T[K];
-}
+export type FunctionReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never;
 
 export type MappedFunction = (...args: any) => any;
 
-export type PromiseReturnType<T> = T extends Promise<infer Return> ? Return : T;
-
-export type ServiceMap = { [service: string]: MethodMap }
-
-//export type Services<T> = { [K in keyof T]: MethodMap<T[K]> }
-
-//export type PickServices<T> = { [P in keyof T as T[P] extends AsyncFunction ? never : P]: T[P] };
-//export type PickServices<T> = { [P in keyof T as T[P] extends Exclude<T[P], AsyncFunction> ? never : P]: T[P] };
-
-//export type PickMethods<T> = { [P in keyof T]: T[P] extends AsyncFunction ? T[P] : never };
-//export type PickMethods<T> = { [P in keyof T as T[P] extends AsyncFunction ? P : never]: T[P] extends AsyncFunction ? T[P] : never };
-
-export type FunctionReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never;
-
-export type MethodMap = { [method: string]: MappedFunction };
+export interface MethodMap { [method: string]: MappedFunction }
 
 export type PrivateMethodMap = { [method: string]: MappedFunction } & { [method: `#${string}`]: MappedFunction };
 
+// export type Services<T> = { [K in keyof T]: MethodMap<T[K]> }
+
+// export type PickServices<T> = { [P in keyof T as T[P] extends AsyncFunction ? never : P]: T[P] };
+// export type PickServices<T> = { [P in keyof T as T[P] extends Exclude<T[P], AsyncFunction> ? never : P]: T[P] };
+
+// export type PickMethods<T> = { [P in keyof T]: T[P] extends AsyncFunction ? T[P] : never };
+// export type PickMethods<T> = { [P in keyof T as T[P] extends AsyncFunction ? P : never]: T[P] extends AsyncFunction ? T[P] : never };
+
+export type PromiseReturnType<T> = T extends Promise<infer Return> ? Return : T;
+
 export type PublicMethodMap = { [method: string]: MappedFunction } & { [method: `#${string}`]: never };
 
+export type RemoveIndexSignature<T> = {
+	[K in keyof T as string extends K
+		? never
+		: number extends K
+			? never
+			: symbol extends K
+				? never
+				: K
+	]: T[K];
+};
+
+export interface ServiceMap { [service: string]: MethodMap }
