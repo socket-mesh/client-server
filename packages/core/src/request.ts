63788221cf00dd8173961e833307b918b9cc745b
Greg Kimmy <greg.kimmy@gmail.com>
2025-11-05 21:58:34 -0600

    fix: updated code to meet lint requirements

--- packages/core/src/request.ts
+++ packages/core/src/request.ts
@@ -1,12 +1,54 @@
-import { MethodMap, PrivateMethodMap, PublicMethodMap, ServiceMap } from "./maps/method-map.js";
-import { toArray } from "./utils.js";
+import { MethodMap, PrivateMethodMap, PublicMethodMap, ServiceMap } from './maps/method-map.js';
+import { toArray } from './utils.js';
 
 export type AnyRequest<
 	TOutgoing extends PublicMethodMap,
 	TPrivateOutgoing extends PrivateMethodMap,
 	TService extends ServiceMap
 > =
-	ServiceRequest<TService> | MethodRequest<TPrivateOutgoing> | MethodRequest<TOutgoing>;
+	MethodRequest<TOutgoing> | MethodRequest<TPrivateOutgoing> | ServiceRequest<TService>;
+
+export interface InvokeMethodRequest<TMethodMap extends MethodMap, TMethod extends keyof TMethodMap> extends TransmitMethodRequest<TMethodMap, TMethod> {
+	ackTimeoutMs: false | number,
+	callback: ((err: Error | null, result?: TMethodMap[TMethod]) => void) | null,
+	cid: number,
+	timeoutId?: NodeJS.Timeout
+}
+
+export interface InvokeServiceRequest<TServiceMap extends ServiceMap, TService extends keyof TServiceMap, TMethod extends keyof TServiceMap[TService]> extends TransmitServiceRequest<TServiceMap, TService, TMethod> {
+	ackTimeoutMs: false | number,
+	callback: ((err: Error | null, result?: TServiceMap[TService][TMethod]) => void) | null,
+	cid: number,
+	timeoutId?: NodeJS.Timeout
+}
+
+export type MethodRequest<TMethodMap extends MethodMap> =
+	{ [TMethod in keyof TMethodMap]:
+		InvokeMethodRequest<TMethodMap, TMethod> | TransmitMethodRequest<TMethodMap, TMethod>
+	}[keyof TMethodMap];
+
+export interface Request {
+	promise: Promise<void>,
+	sentCallback?: (err?: Error) => void
+}
+
+export type ServiceRequest<TServiceMap extends ServiceMap> =
+	{ [TService in keyof TServiceMap]:
+		{ [TMethod in keyof TServiceMap[TService]]:
+			InvokeServiceRequest<TServiceMap, TService, TMethod> | TransmitServiceRequest<TServiceMap, TService, TMethod>
+		}[keyof TServiceMap[TService]]
+	}[keyof TServiceMap];
+
+export interface TransmitMethodRequest<TMethodMap extends MethodMap, TMethod extends keyof TMethodMap> extends Request {
+	data?: Parameters<TMethodMap[TMethod]>[0],
+	method: TMethod
+}
+
+export interface TransmitServiceRequest<TServiceMap extends ServiceMap, TService extends keyof TServiceMap, TMethod extends keyof TServiceMap[TService]> extends Request {
+	data?: Parameters<TServiceMap[TService][TMethod]>[0],
+	method: TMethod,
+	service: TService
+}
 
 export function abortRequest<
 	TOutgoing extends PublicMethodMap,
@@ -22,17 +64,33 @@ export function abortRequest<
 	}
 }
 
+export function isRequestDone<
+	TOutgoing extends PublicMethodMap,
+	TPrivateOutgoing extends PrivateMethodMap,
+	TService extends ServiceMap
+>(request: AnyRequest<TOutgoing, TPrivateOutgoing, TService>): boolean {
+	if ('callback' in request) {
+		return (request.callback === null);
+	}
+
+	return !request.sentCallback;
+}
+
 export class RequestCollection<
 	TOutgoing extends PublicMethodMap,
 	TPrivateOutgoing extends PrivateMethodMap,
 	TService extends ServiceMap
 > {
-	private readonly _requests: AnyRequest<TOutgoing, TPrivateOutgoing, TService>[];
 	private readonly _callbacks: (() => void)[];
+	private readonly _requests: AnyRequest<TOutgoing, TPrivateOutgoing, TService>[];
 
 	constructor(requests: AnyRequest<TOutgoing, TPrivateOutgoing, TService> | AnyRequest<TOutgoing, TPrivateOutgoing, TService>[]) {
 		this._requests = toArray(requests).filter(req => !isRequestDone<TOutgoing, TPrivateOutgoing, TService>(req));
 		this._callbacks = [];
+	}
+
+	public isDone(): boolean {
+		return this._requests.length === 0;
 	}
 
 	public get items(): ReadonlyArray<AnyRequest<TOutgoing, TPrivateOutgoing, TService>> {
@@ -56,77 +114,18 @@ export class RequestCollection<
 		}
 	}
 
-	public isDone(): boolean {
-		return this._requests.length === 0;
-	}
-
 	[Symbol.iterator]() {
-    const values = this._requests;
-    let index = 0;
+		const values = this._requests;
+		let index = 0;
 
-    return {
-      next() {
-        if (index < values.length) {
-          const val = values[index];
-          index++;
-          return { value: val, done: false };
-        } else return { done: true };
-      }
-    };
+		return {
+			next() {
+				if (index < values.length) {
+					const val = values[index];
+					index++;
+					return { done: false, value: val };
+				} else return { done: true };
+			}
+		};
 	}
-}
-
-export type ServiceRequest<TServiceMap extends ServiceMap> =
-	{ [TService in keyof TServiceMap]:
-		{ [TMethod in keyof TServiceMap[TService]]:
-			TransmitServiceRequest<TServiceMap, TService, TMethod> | InvokeServiceRequest<TServiceMap, TService, TMethod> 
-		}[keyof TServiceMap[TService]]
-	}[keyof TServiceMap]
-
-export type MethodRequest<TMethodMap extends MethodMap> =
-	{ [TMethod in keyof TMethodMap]: 
-		TransmitMethodRequest<TMethodMap, TMethod> | InvokeMethodRequest<TMethodMap, TMethod>
-	}[keyof TMethodMap]
-
-
-export interface Request {
-	promise: Promise<void>,
-	sentCallback?: (err?: Error) => void
-}
-
-export interface TransmitServiceRequest<TServiceMap extends ServiceMap, TService extends keyof TServiceMap, TMethod extends keyof TServiceMap[TService]> extends Request {
-	service: TService,
-	method: TMethod,
-	data?: Parameters<TServiceMap[TService][TMethod]>[0]
-}
-
-export interface TransmitMethodRequest<TMethodMap extends MethodMap, TMethod extends keyof TMethodMap> extends Request {
-	method: TMethod,
-	data?: Parameters<TMethodMap[TMethod]>[0]
-}
-
-export interface InvokeServiceRequest<TServiceMap extends ServiceMap, TService extends keyof TServiceMap, TMethod extends keyof TServiceMap[TService]> extends TransmitServiceRequest<TServiceMap, TService, TMethod> {
-	cid: number,
-	ackTimeoutMs: number | false,
-	timeoutId?: NodeJS.Timeout;
-	callback: null | ((err: Error | null, result?: TServiceMap[TService][TMethod]) => void)
-}
-
-export interface InvokeMethodRequest<TMethodMap extends MethodMap, TMethod extends keyof TMethodMap> extends TransmitMethodRequest<TMethodMap, TMethod> {
-	cid: number,
-	ackTimeoutMs: number | false,
-	timeoutId?: NodeJS.Timeout;
-	callback: null | ((err: Error | null, result?: TMethodMap[TMethod]) => void)
-}
-
-export function isRequestDone<
-	TOutgoing extends PublicMethodMap,
-	TPrivateOutgoing extends PrivateMethodMap,
-	TService extends ServiceMap
->(request: AnyRequest<TOutgoing, TPrivateOutgoing, TService>): boolean {
-	if ('callback' in request) {
-		return (request.callback === null);
-	}
-
-	return !request.sentCallback;
 }
--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,11 +1,11 @@
+export function toArray<T>(arr: T | T[]): T[] {
+	return Array.isArray(arr) ? arr : [arr];
+}
+
 export function wait(duration: number): Promise<void> {
 	return new Promise<void>((resolve) => {
 		setTimeout(() => {
 			resolve();
 		}, duration);
 	});
-}
-
-export function toArray<T>(arr: T | T[]): T[] {
-	return Array.isArray(arr) ? arr : [arr];
 }
